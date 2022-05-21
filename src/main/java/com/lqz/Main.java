9f0a2b791daee5742ad117d3e37154736085c27f
LQZ <907877767@qq.com>
2022-05-21 15:21:18 +0800

    初版

--- src/main/java/com/lqz/Main.java
+++ src/main/java/com/lqz/Main.java
@@ -1,6 +1,11 @@
 package com.lqz;
 
+import com.lqz.custom.BaiduUploadTask;
+import com.lqz.custom.BaiduUploadTaskBuilder;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author LQZ
@@ -10,6 +15,23 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        // 创建上传任务
+        BaiduUploadTaskBuilder builder = new BaiduUploadTaskBuilder();
+        builder.setAccessToken("百度网盘的授权码")
+                .setFile(new File("上传的文件"))
+                .setAppPath("需要保存在网盘的哪个位置");
+        BaiduUploadTask task = builder.build();
 
+        // 设置上传参数
+        task.setIsDir(0)
+                .setRType(0)
+                .setDeleteAll(false);
+
+        // 开始上传
+        List<String> result = task.startUpload();
+        String secondLink = result.get(0);
+        String fsId = result.get(1);
+        System.out.println("secondLink = " + secondLink);
+        System.out.println("fsId = " + fsId);
     }
 }
